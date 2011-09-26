71a47c8097c2d83054f324900f7d73678d298c68
Cyril Quintin <cyqui@smile.fr>
2011-09-26 13:48:10 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+
+# Copyright (C) 2011 by Olivier Matz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# IRC settings.
+$topic_title_prefix = "TF2C Pickup :: "
+
+$irc_host = 'multiplay.uk.quakenet.org'
+$irc_port = 6667
+$irc_nick = 'TF2C-Pickup-Bot'
+$irc_authentication_command = ''
+
+# MySQL settings.
+$db_host = 'localhost'
+$db_user = 'pickupbot'
+$db_pass = 'jfSgot9q'
+$db_name = 'pickupbot'
+
+$ready_timeout = 60
+
+$terms_of_use_url = "http://www.tf2connexion.fr/help/index/page/content/r%C3%A8glementpickup"
+
+$classes = ["demoman", "engineer", "heavy", "medic", "pyro", "scout", "sniper", "soldier", "spy"]
+
